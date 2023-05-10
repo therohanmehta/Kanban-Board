793ebc4366f5b1e016bc9398157b54dde1e8d3bc
Chandra-Bhan-Prajapati <prajapatichandrabhan333@gmail.com>
2023-05-10 16:43:17 +0530

    make the addTodoCompoenent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import AddTodo from "./Components/AddTodo/AddTodo";
 
 function App() {
   return (
     <div className="App">
-    
+      <AddTodo />
     </div>
   );
 }
--- src/Components/AddTodo/AddTodo.jsx
+++ src/Components/AddTodo/AddTodo.jsx
@@ -1,1 +1,14 @@
 //Chandra Bhan
+
+import React from "react";
+
+function AddTodo() {
+  return (
+    <div>
+      <h1>Card</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default AddTodo;
