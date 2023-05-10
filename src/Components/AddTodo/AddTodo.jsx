53a329b5e696f70cd14b5eba459fc891a9b9e348
Chandra-Bhan-Prajapati <prajapatichandrabhan333@gmail.com>
2023-05-10 19:35:32 +0530

    Add some icons in todos section

--- src/Components/AddTodo/AddTodo.jsx
+++ src/Components/AddTodo/AddTodo.jsx
@@ -1,12 +1,38 @@
 //Chandra Bhan
 
 import React from "react";
-
+import style from "./AddTodo.module.css";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import AddIcon from "@mui/icons-material/Add";
+import RollerShadesClosedOutlinedIcon from "@mui/icons-material/RollerShadesClosedOutlined";
 function AddTodo() {
   return (
     <div>
       <h1>Card</h1>
-      <div></div>
+      <div className={style.mainCardDiv}>
+        <header>
+          <div>To Do</div>
+          <div>
+            <MoreHorizIcon color="disabled" />
+          </div>
+        </header>
+        <section></section>
+        <footer>
+          <div>
+            <button>
+              <AddIcon
+                sx={{ marginBottom: "-5px" }}
+                fontSize="small"
+                color="disabled"
+              />
+              Add a card
+            </button>
+          </div>
+          <div>
+            <RollerShadesClosedOutlinedIcon fontSize="small" color="disabled" />
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
