40b30277651734adfdbf749fec981c32467767ec
nlprasanna5 <lakshmi.76740@gmail.com>
2023-05-11 11:56:20 +0530

    resolved merrge conflicts of description branch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import AddList from './Components/AddList/AddList';
 import Navbar from './Components/Navbar/Navbar';
+import CustomizedDialogs from './Components/Description/Description';
 
 function App() {
   return (
     <div className="App">
     <Navbar/> 
     <AddList/>
+    <CustomizedDialogs/>
     </div>
   );
 }
