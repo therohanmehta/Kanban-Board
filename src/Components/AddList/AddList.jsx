bce9c33f2a81b25f2b02e3bf2f829f4e0e4c89e7
SamadSRahman <121732577+SamadSRahman@users.noreply.github.com>
2023-05-11 15:52:27 +0530

    CSS added to Add List Component

--- src/Components/AddList/AddList.jsx
+++ src/Components/AddList/AddList.jsx
@@ -1,24 +1,38 @@
-//Samad
-import React, {useState} from 'react'
-import styles from './AddList.module.css'
-import AddIcon from '@mui/icons-material/Add';
-import { Button } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+//SRahman
+import React, { useState } from "react";
+import styles from "./AddList.module.css";
+import AddIcon from "@mui/icons-material/Add";
+import { Button } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
 export default function AddList() {
-    const[isVisible, setIsVisible] = useState(true)
+  const [isVisible, setIsVisible] = useState(true);
+
+  function handleAddList() {
+    setIsVisible(false);
+  }
   return (
-      <div className={styles.container}>
-          {isVisible?(<div className={styles.btnWrapper} onClick={()=>setIsVisible(false)}>
-            <AddIcon/> <span> Add another list</span>
-          </div>) : (
-                  <div className={styles.inputWrapper}>
-                      <input type="text" />
-                      <div className={styles.innerDiv}>
-                      <Button  variant='contained'>Add List</Button>
-                      <CloseIcon onClick={()=>setIsVisible(true)}/>
-                     </div>
-                  </div>
-          )}
+    <div className={`${styles.container} ${isVisible ? "" : styles.expanded}`}>
+      {isVisible ? (
+        <div className={styles.btnWrapper} onClick={handleAddList}>
+          <AddIcon /> <span > Add another list</span>
+        </div>
+      ) : (
+        <div className={styles.inputWrapper}>
+          <input
+            type="text"
+            className={styles.inputField}
+            placeholder="Enter list title..."
+          />
+          <div className={styles.innerDiv}>
+            <Button variant="contained">Add List</Button>
+            <CloseIcon
+              onClick={() => setIsVisible(!isVisible)}
+              id={styles.closeIcon}
+            />
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
