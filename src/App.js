2b5a04d49238c6699fe7b9d1568fcdab6c7e4309
nlprasanna5 <lakshmi.76740@gmail.com>
2023-05-11 11:48:59 +0530

    added description model

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CustomizedDialogs from './Components/Description/Description';
 
 function App() {
   return (
     <div className="App">
-    
+    <CustomizedDialogs/>
     </div>
   );
 }
