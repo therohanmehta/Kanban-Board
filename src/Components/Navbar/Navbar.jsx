a6227f34bed75fef8f4c86414493bd5b2753f54d
SamadSRahman <121732577+SamadSRahman@users.noreply.github.com>
2023-05-18 02:59:09 +0530

    Clear board feature added to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,25 +7,29 @@ import ViewKanbanOutlinedIcon from '@mui/icons-material/ViewKanbanOutlined';
 import RocketLaunchOutlinedIcon from '@mui/icons-material/RocketLaunchOutlined';
 import BoltIcon from '@mui/icons-material/Bolt';
 import FilterListIcon from '@mui/icons-material/FilterList';
-import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
 import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
-
+import { useRecoilState } from 'recoil';
 import style from './Navbar.module.css'
 import { useNavigate } from 'react-router-dom';
+import { list } from '../../recoil/description_atoms/DescriptionAtoms';
 function Navbar() {
 const [showStar,setShowStar]=useState(true)
 const [showBgChange,setshowBgChange]=useState(true)
 const navigate=useNavigate()
-
+  const [listData, setListData] = useRecoilState(list)
+  function handleClearBoard() {
+    setListData([])
+    localStorage.removeItem('listData')
+  }
   return (
     <>
 
       <nav className={style.navBar}>
      <div className={style.left}>
-     <Button  variant='text'>Monday.com</Button>
+     <Button  variant='text' onClick={()=>navigate('/')}>Monday.com</Button>
      {showStar?<Button  onClick={()=>{setShowStar(!showStar)}}><StarBorderIcon/></Button>:<Button  onClick={()=>{setShowStar(!showStar)}}><StarIcon/></Button>}
      <Button sx={{color:'white',textTransform:'none'}} variant='text' startIcon={<PublicIcon />}>Public</Button>  
       <Button  variant='text'>Workspace Visible</Button>
@@ -36,7 +40,7 @@ const navigate=useNavigate()
      
      <div className={style.right}>
      <Button  variant='text' startIcon={<RocketLaunchOutlinedIcon />}>Power-ups</Button>
-      <Button  variant='text'  startIcon={<BoltIcon />}>Automation</Button>
+          <Button variant='text' onClick={handleClearBoard} startIcon={<BoltIcon />}>Clear Board</Button>
       <Button  variant='text' startIcon={<FilterListIcon />}>Filter</Button>
       <img className={style.userImg} src="https://sialifehospital.com/wp-content/uploads/2021/04/testimonial-1.png" alt="" />
       <Button id={style.whiteBtn} variant='contained' startIcon={<PersonAddAltOutlinedIcon />}>Share</Button>
