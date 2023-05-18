e1c696d7b2fb0aaa1042a0d0614876d88ebd5abb
therohanmehta <rohandhn786@gmail.com>
2023-05-18 11:02:32 +0530

    Feat- Add the auto Scroll feature and footer

--- src/customisation/PricingCard.jsx
+++ src/customisation/PricingCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './PricingCard.module.css'
-function PricingCard() {
+function PricingCard(props) {
   return (
     <div className={styles.card}>
     <div className={styles.content}>
-      <div className={styles.title}>Personal edition</div>
-      <div className={styles.price}>$39.99</div>
-      <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at posuere eros. Interdum et malesuada fames ac ante ipsum primis in faucibus.</div>
+      <div className={styles.title}>{props.plan}</div>
+      <div className={styles.price}>{props.price}</div>
+      <div className={styles.description}>{props.desc}</div>
     </div>
     <button className={styles.BuyButton}>
       Buy now
