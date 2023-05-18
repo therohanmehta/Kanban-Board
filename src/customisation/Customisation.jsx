79d68582195993b318b5d800ad4aff5104cfbe05
therohanmehta <rohandhn786@gmail.com>
2023-05-19 02:38:33 +0530

    UI- Fixing overflow issue

--- src/customisation/Customisation.jsx
+++ src/customisation/Customisation.jsx
@@ -29,7 +29,8 @@ function Customisation() {
   const [linkOfWallpaper, setLinkOfWallpaper] = useState("");
   const [currentWallpaper, setCurrentWallpaper] = useRecoilState(wallpaper);
   const image = [
-    "https://wallpaperaccess.com/full/4545909.jpg",
+    "https://wallpaperaccess.com/full/4545909.jpg",'https://cdn.pixabay.com/photo/2017/05/11/11/15/workplace-2303851_1280.jpg',
+    "https://wallpaperaccess.com/full/4545909.jpg",'https://cdn.pixabay.com/photo/2017/05/11/11/15/workplace-2303851_1280.jpg',
     "https://wallpapercave.com/wp/wp6940885.jpg",
     "https://a-static.besthdwallpaper.com/fantasy-world-wallpaper-2880x1620-29897_52.jpg",
     "https://static1.squarespace.com/static/5fe4caeadae61a2f19719512/5fe5c3a9d85eb525301180ed/5ff082ae17af6f5d1930e6bf/1610530333403/Wallpaper+engine+4k.png?format=1500w",
@@ -248,7 +249,7 @@ function Customisation() {
 
         </div>
       <div id="customisation" className={style.heading}>
-        <h1>Choose Background</h1>
+        <h1>Choose <span>Background</span> </h1>
       </div>
       <div className={style.inpDiv}>
         <TextField
@@ -262,6 +263,7 @@ function Customisation() {
           Apply This Image
         </Button>
       </div>
+      
       <div className={style.imageDiv}>
         {image.map((ele, index) => (
           <img
@@ -274,6 +276,8 @@ function Customisation() {
             src={ele}
           />
         ))}
+     
+
       </div>
       <div id="pricing" className={style.pricing}>
         <h1 style={{color:'transparent',margin:'0',padding:'0'}}>.</h1>
