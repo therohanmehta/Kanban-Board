98beac8278cb8c73af93c0126a0ab29c60120d87
SamadSRahman <121732577+SamadSRahman@users.noreply.github.com>
2023-05-18 13:27:22 +0530

    Warnings handled

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,55 +1,100 @@
-import Button from '@mui/material/Button';
-import React, { useState } from 'react'
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import StarIcon from '@mui/icons-material/Star';
-import PublicIcon from '@mui/icons-material/Public';
-import ViewKanbanOutlinedIcon from '@mui/icons-material/ViewKanbanOutlined';
-import RocketLaunchOutlinedIcon from '@mui/icons-material/RocketLaunchOutlined';
-import BoltIcon from '@mui/icons-material/Bolt';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
-import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
-import { useRecoilState } from 'recoil';
-import style from './Navbar.module.css'
-import { useNavigate } from 'react-router-dom';
-import { list } from '../../recoil/description_atoms/DescriptionAtoms';
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
+import BoltIcon from "@mui/icons-material/Bolt";
+import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import { useRecoilState } from "recoil";
+import style from "./Navbar.module.css";
+import { useNavigate } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
+import { list } from "../../recoil/description_atoms/DescriptionAtoms";
 function Navbar() {
-const [showStar,setShowStar]=useState(true)
-const [showBgChange,setshowBgChange]=useState(true)
-const navigate=useNavigate()
-  const [listData, setListData] = useRecoilState(list)
+  const [showStar, setShowStar] = useState(true);
+  const [showBgChange, setshowBgChange] = useState(true);
+  const navigate = useNavigate();
+  const [listData, setListData] = useRecoilState(list);
   function handleClearBoard() {
-    setListData([])
-    localStorage.removeItem('listData')
+    setListData([]);
+    localStorage.removeItem("listData");
   }
   return (
     <>
-
       <nav className={style.navBar}>
-     <div className={style.left}>
-     <Button  variant='text' onClick={()=>navigate('/')}>Monday.com</Button>
-     {showStar?<Button  onClick={()=>{setShowStar(!showStar)}}><StarBorderIcon/></Button>:<Button  onClick={()=>{setShowStar(!showStar)}}><StarIcon/></Button>}
-     <Button sx={{color:'white',textTransform:'none'}} variant='text' startIcon={<PublicIcon />}>Public</Button>  
-      <Button  variant='text'>Workspace Visible</Button>
-      <Button  id={style.whiteBtn} variant='contained' startIcon={<ViewKanbanOutlinedIcon />}>Board  </Button>_
-     { showBgChange?<Button onClick={()=>{setshowBgChange(!showBgChange)}}  variant='text'  startIcon={<AutoAwesomeIcon />}> Edit Board</Button>: 
-      <Button onClick={()=>{navigate('/#customisation')}} variant='contained' id={style.whiteBtn} startIcon={<AddPhotoAlternateIcon />}> Change Background</Button>}
-     </div>
+        <div className={style.left}>
+          <Button variant="text" onClick={() => navigate("/")}>
+            Monday.com
+          </Button>
+          {showStar ? (
+            <Button
+              onClick={() => {
+                setShowStar(!showStar);
+              }}
+            >
+              <StarBorderIcon />
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                setShowStar(!showStar);
+              }}
+            >
+              <StarIcon />
+            </Button>
+          )}
      
-     <div className={style.right}>
-          <Button variant='text' onClick={handleClearBoard} startIcon={<BoltIcon />}>Clear Board</Button>
-     <Button  variant='text' startIcon={<RocketLaunchOutlinedIcon />}>Power-ups</Button>
-      <Button  variant='text' startIcon={<FilterListIcon />}>Filter</Button>
-      <img className={style.userImg} src="https://sialifehospital.com/wp-content/uploads/2021/04/testimonial-1.png" alt="" />
-      <Button id={style.whiteBtn} variant='contained' startIcon={<PersonAddAltOutlinedIcon />}>Share</Button>
-      <Button  variant='text' startIcon={<MoreHorizOutlinedIcon />}/>
-     </div>
-        </nav>    
-    
+          
+          
+        </div>
+
+        <div className={style.right}>
+        <Button
+            sx={{ color: "white", textTransform: "none" }}
+            variant="text"
+            onClick={() => navigate("/")}
+            startIcon={<HomeIcon/>}
+          >
+            Home
+          </Button>
+        {showBgChange ? (
+            <Button
+              onClick={() => {
+                setshowBgChange(!showBgChange);
+              }}
+              variant="text"
+              startIcon={<AutoAwesomeIcon />}
+            >
+              {" "}
+              Edit Board
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                navigate("/#customisation");
+              }}
+              variant="contained"
+              id={style.whiteBtn}
+              startIcon={<AddPhotoAlternateIcon />}
+            >
+              {" "}
+              Change Background
+            </Button>
+          )}
+          <Button
+            variant="text"
+            onClick={handleClearBoard}
+            startIcon={<BoltIcon />}
+          >
+            Clear Board
+          </Button>
+         
+          <Button variant="text" startIcon={<MoreHorizOutlinedIcon />} />
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/Components/description_comments/DescriptionComments.jsx
+++ src/Components/description_comments/DescriptionComments.jsx
@@ -1,142 +1,146 @@
-
-import { useState } from 'react';
-import style from './DescriptionComments.module.css';
-import Button from '@mui/material/Button';
-import { useRecoilState } from 'recoil';
-import { getData } from '../../utils/Services';
+import { useState } from "react";
+import style from "./DescriptionComments.module.css";
+import Button from "@mui/material/Button";
+import { useRecoilState } from "recoil";
+import { getData } from "../../utils/Services";
 import {
-    atomCardName,
-    list,
-    uidOfListItem,
-    atomListUid,
+  list,
+  uidOfListItem,
+  atomListUid,
 } from "../../recoil/description_atoms/DescriptionAtoms";
-import Moment from 'react-moment';
+import Moment from "react-moment";
 
-
-
-import PersonIcon from '@mui/icons-material/Person';
-
+import PersonIcon from "@mui/icons-material/Person";
 
 function DescriptionComments() {
-    const [showComment, setShowComment] = useState(false);
-    const [commentText, setCommentText] = useState('');
+  const [showComment, setShowComment] = useState(false);
+  const [commentText, setCommentText] = useState("");
 
-    const tempList = getData();
+  const tempList = getData();
 
-    const [currentListUid, setCurrentListUid] = useRecoilState(atomListUid);
-    const [listData, setListData] = useRecoilState(list);
-    const [uidOfList, setUidOfList] = useRecoilState(uidOfListItem);
+  const [currentListUid, setCurrentListUid] = useRecoilState(atomListUid);
+  const [listData, setListData] = useRecoilState(list);
+  const [uidOfList, setUidOfList] = useRecoilState(uidOfListItem);
 
-    const commentsListIndex = tempList.findIndex(
-        (ele) => ele.ListId === currentListUid
-    );
-    const CommentsCardIndex = tempList[commentsListIndex].tasks.findIndex(
-        (ele) => ele.cardItemId === uidOfList
-    );
+  const commentsListIndex = tempList.findIndex(
+    (ele) => ele.ListId === currentListUid
+  );
+  const CommentsCardIndex = tempList[commentsListIndex].tasks.findIndex(
+    (ele) => ele.cardItemId === uidOfList
+  );
 
-    const currentComment = tempList[commentsListIndex].tasks[CommentsCardIndex].comments;
-    console.log(tempList[commentsListIndex].tasks[CommentsCardIndex].comments);
-    const [comments, setComments] = useState(currentComment || []);
+  const currentComment =
+    tempList[commentsListIndex].tasks[CommentsCardIndex].comments;
+  const [comments, setComments] = useState(currentComment || []);
 
-
-    function handleComments(e) {
-        e.preventDefault();
-        if (commentText.trim() === '') {
-            return;
-        }
-        const commentTimeValue = {
-            id: new Date(),
-            comment: commentText,
-            time: new Date().getTime()
-        }
-
-        let updatedComments = [...comments, commentTimeValue];
-        setComments(updatedComments);
-        console.log(commentText);
-        console.log(comments);
-
-
-
-        const listIndex = tempList.findIndex(
-            (ele) => ele.ListId === currentListUid
-        );
-        const cardIndex = tempList[listIndex].tasks.findIndex(
-            (ele) => ele.cardItemId === uidOfList
-        );
-        tempList[listIndex].tasks[cardIndex].comments = updatedComments;
-        localStorage.setItem("listData", JSON.stringify(tempList));
-        setListData(tempList);
-        console.log(tempList);
-
-        setCommentText('')
+  function handleComments(e) {
+    e.preventDefault();
+    if (commentText.trim() === "") {
+      return;
     }
+    const commentTimeValue = {
+      id: new Date(),
+      comment: commentText,
+      time: new Date().getTime(),
+    };
 
-    function handleDelete(i) {
-        let removeItems = comments.filter((element, index) => {
-            return index !== i;
-        })
-        setComments(removeItems);
+    let updatedComments = [...comments, commentTimeValue];
+    setComments(updatedComments);
 
-        const listIndex = tempList.findIndex(
-            (ele) => ele.ListId === currentListUid
-        );
-        const cardIndex = tempList[listIndex].tasks.findIndex(
-            (ele) => ele.cardItemId === uidOfList
-        );
-        tempList[listIndex].tasks[cardIndex].comments = removeItems;
-        localStorage.setItem("listData", JSON.stringify(tempList));
+    const listIndex = tempList.findIndex(
+      (ele) => ele.ListId === currentListUid
+    );
+    const cardIndex = tempList[listIndex].tasks.findIndex(
+      (ele) => ele.cardItemId === uidOfList
+    );
+    tempList[listIndex].tasks[cardIndex].comments = updatedComments;
+    localStorage.setItem("listData", JSON.stringify(tempList));
+    setListData(tempList);
 
+    setCommentText("");
+  }
 
-        console.log(tempList);
+  function handleDelete(i) {
+    let removeItems = comments.filter((element, index) => {
+      return index !== i;
+    });
+    setComments(removeItems);
 
-    }
+    const listIndex = tempList.findIndex(
+      (ele) => ele.ListId === currentListUid
+    );
+    const cardIndex = tempList[listIndex].tasks.findIndex(
+      (ele) => ele.cardItemId === uidOfList
+    );
+    tempList[listIndex].tasks[cardIndex].comments = removeItems;
+    localStorage.setItem("listData", JSON.stringify(tempList));
+  }
 
-
-
-    return (
-        <>
-       <PersonIcon className={style.mainComments}/>
-            <div className={style.commentContainer}>
-           
-                {
-                    showComment ?
-                        <div className={style.commentTexts}>
-                            <div className={style.comment}>
-                                <input type='text' placeholder='Write a comment' value={commentText} onChange={(e) => setCommentText(e.target.value)} className={style.comments} />
-                            </div>
-                            <div>
-                                <Button variant='contained' sx={{ width: '18%', ml: 4.7 }} onClick={handleComments}>Save</Button>
-                            </div>
-                        </div>
-                        :
-                        <div className={style.comment}>
-                            <input type='text' placeholder='Write a comment...' onClick={() => setShowComment(!showComment)} className={style.comments} />
-                        </div>
-                }
-               
-                {
-                    comments.map((comment, index) => (
-                        comment && comment.comment &&  comment.comment.trim() !== '' && <div key={comment.id} className={style.commentsContainer}>
-                             <PersonIcon className={style.mainComment}/>
-                            <small className={style.user} ><span>User</span> <Moment fromNow className={style.commentsTime}>{comment.time}</Moment></small>
-                            <div className={style.eachComment}>
-                                {comment.comment}
-                            </div>
-                            <div className={style.updateComment}>
-
-                                <small style={{color:'red'}} onClick={() => handleDelete(index)} className={style.modifyComment}>Delete</small>
-                            </div>
-                           
-                        </div>
-                        
-                    ))
-                }
-
-
+  return (
+    <>
+      <PersonIcon className={style.mainComments} />
+      <div className={style.commentContainer}>
+        {showComment ? (
+          <div className={style.commentTexts}>
+            <div className={style.comment}>
+              <input
+                type="text"
+                placeholder="Write a comment"
+                value={commentText}
+                onChange={(e) => setCommentText(e.target.value)}
+                className={style.comments}
+              />
             </div>
-        {/* </div> */}
-        </>
-    )
+            <div>
+              <Button
+                variant="contained"
+                sx={{ width: "18%", ml: 4.7 }}
+                onClick={handleComments}
+              >
+                Save
+              </Button>
+            </div>
+          </div>
+        ) : (
+          <div className={style.comment}>
+            <input
+              type="text"
+              placeholder="Write a comment..."
+              onClick={() => setShowComment(!showComment)}
+              className={style.comments}
+            />
+          </div>
+        )}
+
+        {comments.map(
+          (comment, index) =>
+            comment &&
+            comment.comment &&
+            comment.comment.trim() !== "" && (
+              <div key={comment.id} className={style.commentsContainer}>
+                <PersonIcon className={style.mainComment} />
+                <small className={style.user}>
+                  <span>User</span>{" "}
+                  <Moment fromNow className={style.commentsTime}>
+                    {comment.time}
+                  </Moment>
+                </small>
+                <div className={style.eachComment}>{comment.comment}</div>
+                <div className={style.updateComment}>
+                  <small
+                    style={{ color: "red" }}
+                    onClick={() => handleDelete(index)}
+                    className={style.modifyComment}
+                  >
+                    Delete
+                  </small>
+                </div>
+              </div>
+            )
+        )}
+      </div>
+    </>
+  );
 }
 
 export default DescriptionComments;
